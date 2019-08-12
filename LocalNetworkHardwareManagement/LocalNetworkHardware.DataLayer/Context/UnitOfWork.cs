f6eb366e1e1b845dddd8b235dad551c264b3ab98
S.Vahid Najafi <vahid.najafi632@gmail.com>
2019-08-12 14:54:24 +0100

    Systems Repository Created, Unit Of Work Modified

--- LocalNetworkHardwareManagement/LocalNetworkHardware.DataLayer/Context/UnitOfWork.cs
+++ LocalNetworkHardwareManagement/LocalNetworkHardware.DataLayer/Context/UnitOfWork.cs
@@ -3,14 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LocalNetworkHardware.DataLayer.Services;
+using LocalNetworkHardware.DataLayer.Services.Classes;
 
 namespace LocalNetworkHardware.DataLayer.Context
 {
     public class UnitOfWork : IDisposable
     {
-        LocalNetworkHardwareManagement_DBEntities db = 
+        LocalNetworkHardwareManagement_DBEntities db =
             new LocalNetworkHardwareManagement_DBEntities();
 
+        private ISystemsRepository _systemsRepository;
+        public ISystemsRepository SystemsRepository
+        {
+            get
+            {
+                if (_systemsRepository == null)
+                    _systemsRepository = new SystemsRepository(db);
+                return _systemsRepository;
+
+            }
+
+        }
 
 
 
---
+++ LocalNetworkHardwareManagement/LocalNetworkHardware.DataLayer/Services/Classes/SystemsRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalNetworkHardware.DataLayer.Services.Classes
+{
+    public class SystemsRepository: GenericRepository<Systems>, ISystemsRepository
+    {
+        public SystemsRepository(LocalNetworkHardwareManagement_DBEntities context)
+        : base(context)
+        {
+            
+        }
+
+
+    }
+}
