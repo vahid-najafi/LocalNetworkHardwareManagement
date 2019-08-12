630c04caf611a041a28df05f89b6001b46a7be8d
S.Vahid Najafi <vahid.najafi632@gmail.com>
2019-08-12 14:48:29 +0100

    Unit Of Work Created

---
+++ LocalNetworkHardwareManagement/LocalNetworkHardware.DataLayer/Context/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalNetworkHardware.DataLayer.Context
+{
+    public class UnitOfWork : IDisposable
+    {
+        LocalNetworkHardwareManagement_DBEntities db = 
+            new LocalNetworkHardwareManagement_DBEntities();
+
+
+
+
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            db.Dispose();
+        }
+    }
+}
